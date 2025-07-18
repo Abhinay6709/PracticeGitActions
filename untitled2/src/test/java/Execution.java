5ee30816fccef143d958148b3947464abc6e8a0d
Abhina6709 <abhinay@example.com>
2025-07-18 11:45:39 +0530

    intial commit

---
+++ untitled2/src/test/java/Execution.java
@@ -0,0 +1,12 @@
+
+
+import org.corframweork.MyTest;
+import org.corframweork.Test;
+
+public class Execution {
+
+public void main(String []args){
+ Test test=new Test();
+ test.intialialiseTest();
+}
+}
